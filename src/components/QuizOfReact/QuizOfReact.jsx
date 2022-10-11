851d0989d70852270871d9697b56432ad7c7f6ea
Programmerbikash <bikash19081998dey@gmail.com>
2022-10-11 20:33:30 +0600

    quiz

--- src/components/QuizOfReact/QuizOfReact.jsx
+++ src/components/QuizOfReact/QuizOfReact.jsx
@@ -6,7 +6,7 @@ const QuizOfReact = ({reactquez}) => {
     const { question, options } = reactquez;
     // console.log(reactquez);
     return (
-        <div className='quiz-react bg-amber-400 w-5/6 mx-auto mt-3'>
+        <div className='quiz-react bg-amber-400 w-5/6 mx-auto mt-3 rounded-3xl'>
             <h1 className='pt-4 text-indigo-500 px-4 text-2xl font-semibold mt-3'>Quiz: {question}</h1>
             <div className='py-5 flex-1'>
             {
