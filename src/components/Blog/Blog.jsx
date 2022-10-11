9ff9307043d6a5ff6dfb80f64fe7ba2c93930311
Programmerbikash <bikash19081998dey@gmail.com>
2022-10-11 21:11:03 +0600

    blog complete

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -23,7 +23,7 @@ const Blog = () => {
                 <div className="collapse-title text-3xl font-medium text-orange-500">
                     <h1>When should i use useRef?</h1>
                 </div>
-                <div className="collapse-content text-emerald-400 "> 
+                <div className="collapse-content text-emerald-400 font-semibold"> 
                     <p><span className='text-xl font-semibold'>useRef:</span> returns a mutable ref object whose .current property is initialized to the passed argument initialValue. The returned object will persist for the full lifetime of the component. Essentially, useRef is like a “box” that can hold a mutable value in its .current property. You might be familiar with refs primarily as a way to access the DOM. If you pass a ref object to React with, React will set its .current property to the corresponding DOM node whenever that node changes. However, useRef is useful for more than the ref attribute. It’s handy for keeping any mutable value around similar to how you’d use instance fields in classes. This works because useRef creates a plain JavaScript object. The only difference between useRef and creating a current: ... object yourself is that useRef will give you the same ref object on every render. Keep in mind that useRef doesn’t notify you when its content changes. Mutating the .current property doesn’t cause a re-render. If you want to run some code when React attaches or detaches a ref to a DOM node, you may want to use a callback ref instead.</p>
                 </div>
             </div>
